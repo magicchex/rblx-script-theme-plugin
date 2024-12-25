7f73c54d61a21e2a96471fbd30923dc2bb58fff5
GitHub <actions@github.com>
2024-12-25 05:02:47 +0000

    Built and published by Moonwave

--- assets/js/2f5df25b.6c6514e8.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[437],{4410:e=>{e.exports=JSON.parse('{"sourceUrl":"https://github.com/magicchex/roblox-template/blob/master","baseUrl":"/rblx-script-theme-plugin/","classOrder":[],"apiCategories":[]}')}}]);
